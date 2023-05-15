497c22cbaaa19016679e5740207dc053c187b120
elwirrka <rakowska.a@gmail.com>
2023-05-15 11:36:43 +0200

    responsivness and ResizeObserver refactor

--- src/pages/home-page/components/faq-component/faq-component.js
+++ src/pages/home-page/components/faq-component/faq-component.js
@@ -3,12 +3,28 @@ export const FaqComponent = () => {
     const faqAllQuestions = faqComponent.querySelectorAll('.faq-question-partial');
 
     faqAllQuestions.forEach((faqSingleQuestion) => {
-        const faqAnswer = faqSingleQuestion.querySelector('.faq-question-partial__answer');
-        
-        faqSingleQuestion.style.setProperty('--faq-answer-height', `${faqAnswer.scrollHeight}px`);
+        new ResizeObserver(listenToQuestionResize).observe(faqSingleQuestion);
 
         faqSingleQuestion.addEventListener('click', () => {
+            foldAllSiblingsQuestions(faqSingleQuestion);
+
             faqSingleQuestion.toggleAttribute('data-question-expanded');
         });
     });
+
+    function listenToQuestionResize(faqQuestionToListen) {
+        const faqQuestion = faqQuestionToListen[0].target;
+
+        const faqAnswer = faqQuestion.querySelector('.faq-question-partial__answer');
+
+        faqQuestion.style.setProperty('--faq-answer-height', `${faqAnswer.scrollHeight}px`);
+    }
+
+    function foldAllSiblingsQuestions(clickedQuestion) {
+        faqAllQuestions.forEach((faqQuestionToFold) => {
+            if (faqQuestionToFold != clickedQuestion) {
+                faqQuestionToFold.removeAttribute('data-question-expanded');
+            }
+        });
+    }
 }
